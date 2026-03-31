e92da62c64f86b4db1b4694dc9a724fb49a11dbb
axmattos20-lab <ax.mattos20@gmail.com>
2026-03-31 00:10:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def run_bot():
     import schedule
     import time
     # 🔥 TESTE TEMPORÁRIO (coloque aqui)
-        enviar_whatsapp()
+        #enviar_whatsapp()
 
     schedule.every().day.at("10:00").do(enviar_whatsapp)
 
